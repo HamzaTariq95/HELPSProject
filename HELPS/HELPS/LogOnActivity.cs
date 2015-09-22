22b943bb46226b827383e3552615bcf9a525c558
thekingofcheese <boyothekingofcheese@gmail.com>
2015-09-23 02:44:18 +1000

    Added "Forgotten Password" functionality
    When clicked, open UTS reset password page for users.

--- HELPS/HELPS/LogOnActivity.cs
+++ HELPS/HELPS/LogOnActivity.cs
@@ -14,7 +14,7 @@ using Android.Graphics;
 
 namespace HELPS
 {
-    [Activity(Label = "Log On", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, Theme = "@style/helpsFullscreenTheme")]
+    [Activity(Label = "UTS:HELPS", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, Theme = "@style/helpsFullscreenTheme")]
     public class LogonActivity : Activity
     {
         protected override void OnCreate(Bundle bundle)
@@ -24,17 +24,28 @@ namespace HELPS
             // Sets the layout to the "Log On" layout
             SetContentView(Resource.Layout.LogOn);
 
+            // Sets the font for the title to Din Regular
             TextView title = FindViewById<TextView>(Resource.Id.textUtsHelps);
             Typeface titleFont = Typeface.CreateFromAsset(this.Assets, "fonts/din-regular.ttf");
 
             title.SetTypeface(titleFont, TypefaceStyle.Normal);
 
+            // Works the "Forgotten Password" button
+            Button forgottenPasswordButton = FindViewById<Button>(Resource.Id.buttonForgotPassword);
+            forgottenPasswordButton.Click += delegate
+            {
+                // Opens external UTS website where users can change their password
+                Android.Net.Uri uri = Android.Net.Uri.Parse("https://email.itd.uts.edu.au/webapps/myaccount/passwordreset/");
+                Intent intent = Intent.CreateChooser(new Intent(Intent.ActionView, uri), "Open with");
+                StartActivity(intent);
+            };
+
             // Works the "About HELPS" button
-            var aboutHelpsButton = FindViewById<Button>(Resource.Id.buttonWhatHelps);
+            Button aboutHelpsButton = FindViewById<Button>(Resource.Id.buttonWhatHelps);
             aboutHelpsButton.Click += (IntentSender, e) =>
             {
                 // Creates the alert displaying the "About HELPS" information
             };
         }
     }
-}
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
