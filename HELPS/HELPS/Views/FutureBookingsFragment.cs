92562afec6d1f12ddf4c8b3806ea959d1094e197
93asad <94asad@gmail.com>
2015-10-14 00:28:33 +1100

    Future bookings done

--- HELPS/HELPS/Views/FutureBookingsFragment.cs
+++ HELPS/HELPS/Views/FutureBookingsFragment.cs
@@ -10,12 +10,14 @@ using Android.Runtime;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using Newtonsoft.Json;
 using HELPS.Model;
 
 namespace HELPS.Views
 {
     public class FutureBookingsFragment : Fragment
     {
+        private StudentData studentData;
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -26,29 +28,62 @@ namespace HELPS.Views
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             View view = inflater.Inflate(Resource.Layout.MainLayout, container, false);
-            
+
+            //Get student data from intent in parent activity
+            studentData = JsonConvert.DeserializeObject<StudentData>(this.Activity.Intent.GetStringExtra("student"));
+
             // Set the "Upcoming Sessions" list view to display (upto) the four closest sessions
             DisplayUpcomingBookings(view);
+
+            DisplayUserName(view);
 
             return view;
         }
 
         private void DisplayUpcomingBookings(View view)
         {
+            SessionController sessionController = new SessionController();
+            SessionBookingData sessionBookingData = sessionController.GetSessionBookingData(studentData.attributes.studentID);
+            List<Booking> bookings = new List<Booking>();
+
+            if (sessionBookingData == null)
+            {
+                //Display on screen: no bookings found
+            }
+            else
+            {
+                addSessionBookingToList(sessionBookingData, bookings);
+            }
             // {Architecture} change the code to generate the list view data from the user's data
-            List<Booking> sessionsList = new List<Booking>();
 
-            sessionsList.Add(new SessionBooking(false, Convert.ToDateTime("01/01/2015"), "B1.05.202", "Mr Tutor", "type"));
-            sessionsList.Add(new SessionBooking(false, Convert.ToDateTime("01/01/2015"), "B1.05.202", "Mr Tutor", "type"));
-            sessionsList.Add(new SessionBooking(false, Convert.ToDateTime("01/01/2015"), "B1.05.202", "Mr Tutor", "type"));
 
-            sessionsList.Add(new WorkshopBooking(1, Convert.ToDateTime("01/01/2015"), 123, 456));
-            sessionsList.Add(new WorkshopBooking(1, Convert.ToDateTime("01/01/2015"), 123, 456));
-            sessionsList.Add(new WorkshopBooking(1, Convert.ToDateTime("01/01/2015"), 123, 456));
+
+            //bookings.Add(new SessionBooking(false, Convert.ToDateTime("01/01/2015"), "B1.05.202", "Mr Tutor", "type"));
+            //bookings.Add(new SessionBooking(false, Convert.ToDateTime("01/01/2015"), "B1.05.202", "Mr Tutor", "type"));
+
+            bookings.Add(new WorkshopBooking(1, Convert.ToDateTime("01/01/2015"), 123, 456));
+            bookings.Add(new WorkshopBooking(1, Convert.ToDateTime("01/01/2015"), 123, 456));
+            bookings.Add(new WorkshopBooking(1, Convert.ToDateTime("01/01/2015"), 123, 456));
 
             ListView upcomingList = view.FindViewById<ListView>(Resource.Id.listUpcoming);
+            upcomingList.Adapter = new BookingBaseAdapter(Activity, bookings);
+        }
 
-            upcomingList.Adapter = new BookingBaseAdapter(this.Activity, sessionsList);
+        private void addSessionBookingToList(SessionBookingData sessionBookingData, List<Booking> bookings)
+        {
+            foreach (SessionBooking sessionBooking in sessionBookingData.attributes)
+            {
+                bookings.Add(sessionBooking);
+            }
+        }
+
+        private void DisplayUserName(View view)
+        {
+            // {Architecture} Get from the database.
+            string helloUser = GetString(Resource.String.hello) + " " + studentData.attributes.studentID + "!";
+            TextView helloUserText = view.FindViewById<TextView>(Resource.Id.textHelloUser);
+
+            helloUserText.Text = helloUser;
         }
     }
 }
