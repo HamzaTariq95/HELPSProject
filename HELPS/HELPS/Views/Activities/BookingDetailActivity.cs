dadeb6a5f5c032d7e15d3951bc3f0b42c16aa2f9
thekingofcheese <boyothekingofcheese@gmail.com>
2015-10-16 19:36:59 +1100

    BookingDetail things

--- HELPS/HELPS/Views/Activities/BookingDetailActivity.cs
+++ HELPS/HELPS/Views/Activities/BookingDetailActivity.cs
@@ -9,17 +9,31 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using SupportToolbar = Android.Support.V7.Widget.Toolbar;
+using Android.Support.V7.App;
 
 namespace HELPS.Views.Activities
 {
     [Activity(Label = "BookingDetailActivity")]
-    public class BookingDetailActivity : Activity
+    public class BookingDetailActivity : AppCompatActivity
     {
+        private SupportToolbar _Toolbar;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            // Create your application here
+            // Set our view from the "BookingDetail" layout resource.
+            SetContentView(Resource.Layout.BookingDetail);
+
+            // Set the toolbar
+            _Toolbar = FindViewById<SupportToolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(_Toolbar);
+
+            // {Architecture} Pass the workshop title as the toolbar title
+            // SupportActionBar.Title(<pass here>)
+
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
         }
     }
 }
