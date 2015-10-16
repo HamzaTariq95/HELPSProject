a21b41637ec4837b6fdcf7555ea018e938161348
thekingofcheese <boyothekingofcheese@gmail.com>
2015-10-16 21:08:47 +1100

    Set up framework for buttons

--- HELPS/HELPS/Views/Activities/BookingDetailActivity.cs
+++ HELPS/HELPS/Views/Activities/BookingDetailActivity.cs
@@ -19,6 +19,8 @@ namespace HELPS.Views.Activities
     public class BookingDetailActivity : AppCompatActivity
     {
         private SupportToolbar _Toolbar;
+        private LinearLayout _Booked;
+        private LinearLayout _NotBooked;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -36,15 +38,41 @@ namespace HELPS.Views.Activities
 
             SupportActionBar.SetDisplayHomeAsUpEnabled(true);
 
-            // Set up notification button
-            Button changeNotificationButton = FindViewById<Button>(Resource.Id.buttonChangeNotification);
-            changeNotificationButton.Click += delegate
-            {
-                DisplayNotificationSettings();
-            };
+            _Booked = FindViewById<LinearLayout>(Resource.Id.bookedButtons);
+            _Booked.Visibility = ViewStates.Gone;
 
-            //Set up cancel button
-            Button cancelButton = FindViewById<Button>(Resource.Id.buttonCancelBooking);
+            _NotBooked = FindViewById<LinearLayout>(Resource.Id.notBookedButtons);
+            _NotBooked.Visibility = ViewStates.Gone;
+
+            // Send an intent that tells the activity if the session is booked by the student or not.
+            // if (booked)
+            // {
+                    // _Booked.Visibility = ViewStates.Visible        
+
+                    // Set up notification button
+                    Button changeNotificationButton = FindViewById<Button>(Resource.Id.buttonChangeNotification);
+                    changeNotificationButton.Click += delegate
+                    {
+                        DisplayNotificationSettings();
+                    };
+
+                    //Set up cancel button
+                    Button cancelButton = FindViewById<Button>(Resource.Id.buttonCancelBooking);
+                    cancelButton.Click += delegate
+                    {
+                        CancelBooking();
+                    };
+            // }
+            // else
+            // {
+            // Display the booking button
+                    // _NotBooked.Visibility = ViewStates.Visible 
+                    // Button bookButton = FindViewById<Button>(Resource.Id.buttonBook)
+                    // bookButton.Click += delegate
+                    // {
+                    //      Book();
+                    // }
+            // }
         }
 
         // Sends the user to the notification settings
@@ -67,6 +95,11 @@ namespace HELPS.Views.Activities
             });
             cancelAlert.SetNegativeButton("NO", delegate { });
             cancelAlert.Show();
+        }
+
+        private void Book()
+        {
+            // Code to book the workshop
         }
     }
 }
